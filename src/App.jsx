06732f3f015d387d17a611c4402529d10540f3c1
Sucheta <sucheta.pal3@gmail.com>
2023-11-29 22:43:54 +0530

    Display player X when clicking the square

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 import './style.scss';
 import Board from './components/Board';
 
 function App() {
+  const [counter, setCounter] = useState(1);
+
+  const onBtnClick = () => {
+    setCounter(currentCounter => {
+      return currentCounter + 1;
+    });
+  };
+
   return (
     <div className="app">
       <Board />
+      <div>
+        <button onClick={onBtnClick}>Click me please</button>
+        <div>{counter}</div>
+      </div>
     </div>
   );
 }
