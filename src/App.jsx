95a97315852bbe2a6f5d08bcdb6ce249786168b9
Sucheta <sucheta.pal3@gmail.com>
2023-12-01 14:25:03 +0530

    Added players X and )

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,21 @@ import './style.scss';
 import Board from './components/Board';
 
 function App() {
-  const [counter, setCounter] = useState(1);
+  // const [counter, setCounter] = useState(1);
 
-  const onBtnClick = () => {
-    setCounter(currentCounter => {
-      return currentCounter + 1;
-    });
-  };
+  // const onBtnClick = () => {
+  //   setCounter(currentCounter => {
+  //     return currentCounter + 1;
+  //   });
+  // };
 
   return (
     <div className="app">
       <Board />
-      <div>
+      {/* <div>
         <button onClick={onBtnClick}>Click me please</button>
         <div>{counter}</div>
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,16 +3,24 @@ import Square from './Square';
 
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
 
   const handleSquareClick = clickedPosition => {
+    //The Values can be either null, X, O
+    if (squares[clickedPosition]) {
+      return;
+    }
+
     setSquares(currentSquares => {
       return currentSquares.map((squareValue, position) => {
         if (clickedPosition === position) {
-          return 'X';
+          return isXNext ? 'X' : 'O';
         }
         return squareValue;
       });
     });
+
+    setIsXNext(currentIsXNext => !currentIsXNext);
   };
 
   const renderSquare = position => {
